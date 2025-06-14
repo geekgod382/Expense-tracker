d7795cf4b9f0911208047c37c2632eb21bda3b67
Manan Sharma <manan140306@gmail.com>
2025-06-14 12:53:22 +0530

    Update views.py

--- ExpenseTracker/expenses/views.py
+++ ExpenseTracker/expenses/views.py
@@ -4,12 +4,19 @@ from .forms import ExpenseForm
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from datetime import datetime
+from django.db.models import Sum
+import json
 
 # Create your views here.
 @login_required
 def home(request):
     expenses = Expense.objects.filter(user=request.user)
     total = sum(exp.amount for exp in expenses)
+
+    category_data = expenses.values('category').annotate(total = Sum('amount'))
+
+    categories = [entry['category'] for entry in category_data]
+    totals = [entry['total'] for entry in category_data]
     
     # Calculate monthly total
     current_month = timezone.now().month
@@ -20,7 +27,9 @@ def home(request):
     return render(request, 'expenses/home.html', {
         'expenses': expenses,
         'total': total,
-        'monthly_total': monthly_total
+        'monthly_total': monthly_total,
+        'totals_json' : json.dumps(totals),
+        'categories_json' : json.dumps(categories)
     })
 
 @login_required
@@ -39,3 +48,12 @@ def add_expense(request):
 @login_required
 def logout(request):
     return render(request, 'expenses/logout.html')
+
+@login_required
+def delete(request, expense_id):
+    try:
+        expense = Expense.objects.get(id=expense_id, user=request.user)
+        expense.delete()
+        return redirect('expense:home')
+    except Expense.DoesNotExist:
+        return redirect('expense:home')
