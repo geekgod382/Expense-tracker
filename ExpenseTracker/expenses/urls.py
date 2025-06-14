7ab21a60e8a91b48dbd84e380766e268a3a444c3
Manan Sharma <manan140306@gmail.com>
2025-06-14 12:54:16 +0530

    Update urls.py

--- ExpenseTracker/expenses/urls.py
+++ ExpenseTracker/expenses/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "expense"
 urlpatterns=[
     path('',views.home,name='home'),
-    path('add/', views.add_expense, name = 'add_expense')
+    path('add/', views.add_expense, name = 'add_expense'),
+    path('delete/<int:expense_id>/', views.delete, name='delete')
 ]
